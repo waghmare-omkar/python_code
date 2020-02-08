5b723f4e09afce3e172800a4ed936ae3dfa1aa87
Omkar Waghmare <waghmare@umich.edu>
2020-02-07 23:54:28 -0500

    Moved all vlog files to specific folders

--- Vlog_app/vlog_app.py
+++ Vlog_app/vlog_app.py
@@ -17,9 +17,11 @@ from datetime import datetime
 
 # Lets create a class for app
 
+PATH = 'C:\\Users\\op95w\\github\\python_code\\Vlog_app\\'
+
 
 class AppGUI:
-    def __init__(self, window_title, widthXheight="700x540"):
+    def __init__(self, window_title, iconFileName, widthXheight="700x540"):
 
         self.app_window = tk.Tk()
 
@@ -27,7 +29,7 @@ class AppGUI:
         self.app_window.title(window_title)
 
         # Set the icon
-        # self.app_window.iconbitmap(iconFileName)
+        self.app_window.iconbitmap(iconFileName)
 
         # Set aspect ratio
         self.app_window.geometry(widthXheight)
@@ -38,8 +40,7 @@ class AppGUI:
         # Create a canvas that fits the video
         self.frame1 = tk.Frame(self.app_window)
         self.frame1.pack()
-        self.canvas = tk.Canvas(self.frame1, width=self.video.width,
-                                height=self.video.height)
+        self.canvas = tk.Canvas(self.frame1, width=self.video.width, height=self.video.height)
         self.canvas.pack(padx=10, side=tk.LEFT)
 
         # Set what happens when closing
@@ -50,29 +51,23 @@ class AppGUI:
         self.frame2 = tk.Frame(self.app_window)
         self.frame2.pack()
         # Creating a photoimage object to use image in button
-        # rec_photo1 = PIL.ImageTk.PhotoImage(
-        #    file="Vlog_Master_Files\\record_icon.ico")
-        self.button_rec = tk.Button(self.frame2, text="Record Vlog",
-                                    compound="left")
+        rec_photo1 = PIL.ImageTk.PhotoImage(
+            file=PATH+"record_icon.ico")
+        self.button_rec = tk.Button(self.frame2, text="Record Vlog", image=rec_photo1, compound="left")
         self.button_rec.state = False
         self.button_rec_prev = False
         self.button_rec.bind('<ButtonRelease-1>', self.manage_recording)
         self.button_rec.pack(ipadx=5, padx=10, side=tk.LEFT)
 
         # Creating a label for displaying current status
-        self.label_bottom = tk.Label(
-            self.app_window, text="Press the record button to start vlogging")
+        self.label_bottom = tk.Label(self.app_window, text="Press the record button to start vlogging")
         self.label_bottom.place(relx=1.0, rely=1.0, anchor='se')
 
         # Creating one more button for face detection toggle
-        # rec_photo2 = PIL.ImageTk.PhotoImage(
-        #    file="Vlog_Master_Files\\face_icon.ico")
-        self.button_face_detect = tk.Button(
-            self.frame2, text="Detect Faces",
-            compound="left")
+        rec_photo2 = PIL.ImageTk.PhotoImage(file=PATH+"face_icon.ico")
+        self.button_face_detect = tk.Button(self.frame2, text="Detect Faces", image=rec_photo2, compound="left")
         self.button_face_detect.state = False
-        self.button_face_detect.bind(
-            '<ButtonRelease-1>', self.toggle_face_detection)
+        self.button_face_detect.bind('<ButtonRelease-1>', self.toggle_face_detection)
         self.button_face_detect.pack(ipadx=5, padx=10, side=tk.LEFT)
 
         # Create new folder for recordings
@@ -98,8 +93,7 @@ class AppGUI:
         if ret:
             if self.button_face_detect.state is True:
                 # First thing is detect face
-                faces = self.video.face_cascade.detectMultiScale(
-                    cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), 1.3, 5)
+                faces = self.video.face_cascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY), 1.3, 5)
                 for (x, y, w, h) in faces:
                     cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
@@ -119,10 +113,8 @@ class AppGUI:
             thickness = 1
 
             # Using cv2.putText() method
-            frame = cv2.putText(frame, text_date, org_date, font, fontScale,
-                                color, thickness, cv2.LINE_AA, False)
-            frame = cv2.putText(frame, text_time, org_time, font, fontScale,
-                                color, thickness, cv2.LINE_AA, False)
+            frame = cv2.putText(frame, text_date, org_date, font, fontScale, color, thickness, cv2.LINE_AA, False)
+            frame = cv2.putText(frame, text_time, org_time, font, fontScale, color, thickness, cv2.LINE_AA, False)
 
             # Add frame lines to video
             cv2.line(img=frame, pt1=(575, 75), pt2=(625, 75), color=(
@@ -179,7 +171,7 @@ class AppGUI:
             # Define the codec and create VideoWriter object
             fourcc = cv2.VideoWriter_fourcc(*'XVID')
             # Current recording file
-            timestampStr = "\Vlog_" + datetime.now(
+            timestampStr = "\\Vlog_" + datetime.now(
             ).strftime("%d-%b-%Y_%H-%M-%S")+".avi"
             self.current_rec_file = self.dirName + timestampStr
             # Write to recording using video writer
@@ -222,7 +214,7 @@ class captureVideo:
 
         # Initialize object detection cascades
         self.face_cascade = cv2.CascadeClassifier(
-            'haarcascade_frontalface_default.xml')
+            PATH + 'haarcascade_frontalface_default.xml')
 
     def __del__(self):
         if self.capturedVideo.isOpened():
@@ -241,4 +233,4 @@ class captureVideo:
 
 
 # Lets run the app
-AppGUI("Vlog Master",  "700x540")
+AppGUI("Vlog Master", PATH+"favicon.ico", "700x540")
